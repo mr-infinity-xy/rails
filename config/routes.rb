d13d73c0201632bb4c3be02460f1a5eae63cd754
Rohan <rohanpujaris@gmail.com>
2014-06-08 13:25:14 +0530

    Delete added

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
    
-   resources :articles
+   resources :articles, param: :title     #param title is used to send title='' instead of id='' which is the default. Though it doesn't change RHS of hash , which is still valid id. It can be changed by to_param method in article
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
