927603c54a160ba85ace26451575b5f2ee51a4d8
Rohan <rohanpujaris@gmail.com>
2014-07-09 22:32:36 +0530

    07/09 1

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,2 +1,22 @@
-module FormsHelper
+module FormsHelper 
+	def generate_list(input)
+	    if input.is_a? Hash
+	    	output ="" 
+	    	input.each do|parent, child|
+					output += "<li>#{parent}</li>"  
+					if child.is_a? Array
+						output += "<ul class='ul-margin'>"
+						child.each do|array_value|
+							if array_value.is_a? Hash
+								output += generate_list(array_value)
+							elsif array_value.is_a? String
+								output += "<li>#{array_value}</li>"
+							end
+						end
+					end
+					output += "</ul>" 		
+	    	end
+	    end
+	    output.html_safe
+end
 end
